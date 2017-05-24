a94004961a9893f2ceac6c397e5f9321c6948683
Rick Eyre <rick.eyre@outlook.com>
2017-05-24 13:57:39 -0400

    RE - Use Scotiabank eslint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,13 @@
 module.exports = {
   'parser': 'babel-eslint',
   'extends': [
-    'standard',
+    '@scotia/eslint-config-scotiabank',
     'plugin:react/recommended'
   ],
   'plugins': [
-    'standard',
-    'promise',
     'react'
   ],
   'env': {
     'jest': true
-  },
-  'rules': {
-    'semi': [2, 'always'],
-    'object-curly-spacing': [2, 'always'],
-    'array-bracket-spacing': [2, 'always'],
-    'space-before-function-paren': [2, 'never']
   }
 };
